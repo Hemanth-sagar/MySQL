4e86bd76450d3e67cb204fa86f0e9676bd4fbc0e
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-07-17 23:18:48 +0530

    project ‘P1’

--- Employee/queries/count of employees.sql
+++ Employee/queries/count of employees.sql
@@ -1,3 +1,19 @@
+/*
+ o/p:
+    EMPID	FULLNAME	   MANAGERID	CITY
+     121	john snow	     321	    Toronto
+     321	Walter White	 986	    California
+     421	Kuldeep rana	 876	    New Delhi
+     
+    EMPID	PROJECTID	SALARY	VARIABLE
+    121	        p1	     8000	  500
+    321	        p2	     10000	  1000
+    421	        p1	     12000	   0
+*/
+
+
+
+
 select count('p1') from emps where projectid='p1';
 select count(*) from emps group by projectid having projectid='p1';
 select count(*) from emps where projectid='p1';
